70a1728187ce2cb383cb73153674e4961ed6a305
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-05-20 21:53:07 +0200

    Add project files.

---
+++ TestCultureDotComma/MainWindowViewmodel.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestCultureDotComma
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private double _numero; public double Numero { get => _numero; set {  Set(() => Numero, ref _numero, value); }}
+
+       
+        public MainWindowViewModel()
+        {
+            Numero = 1;
+        }
+
+    }
+
+
+}
---
+++ TestCultureDotComma/SmartCommaConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+
+namespace TestCultureDotComma
+{
+    internal class SmartCommaConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString().Replace(",", ".");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString().Replace(",", ".");
+
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
